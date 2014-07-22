80236c44b2f26651680c3b4bb1f8e1e742b15a74
Bertjan Broeksema <dev@broeksemaatjes.nl>
2014-07-22 21:52:50 +0200

    Rename the R script to plot1.R and directly write the plot to file.

--- exploratory-analysis/project-2/plot1.R
+++ exploratory-analysis/project-2/plot1.R
@@ -4,11 +4,13 @@
 
 source("prepareData.R")
 totalEmissionForAllSources <- NEI[,list(Emission = sum(Emissions)), by = year]
-myTicks <- c(3500000, 4000000, 5000000, 6000000, 7000000)
+ticks <- c(3500000, 4000000, 5000000, 6000000, 7000000, 7500000)
+png(filename="plot1.png")
 with(totalEmissionForAllSources, {
      plot(year, Emission, type='n', main = "Total Emissions by Year",
-          yaxt="n", ylim = range(myTicks), ylab = "Emissions (Milions of tons)")
-     axis(2, at=myTicks, labels=c('3.5M', '4M', '5M','6M','7M'))
+          yaxt="n", ylim = range(ticks), ylab = "Emissions (Milions of tons)")
+     axis(2, at = ticks, labels=c('3.5M', '4M', '5M','6M','7M', '7.5M'))
      lines(year, Emission)
   }
 )
+dev.off()
